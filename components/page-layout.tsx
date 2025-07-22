e8d5df50b236ff5c85953fdadd1e225a94e28d29
Aadi Katyal <52411543+aadikatyal@users.noreply.github.com>
2025-07-21 23:36:38 -0400

    added logo to header

--- components/page-layout.tsx
+++ components/page-layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react"
+import Image from "next/image";
 import { Navbar } from "@/components/navbar"
 import { Footer } from "@/components/footer"
 
@@ -11,10 +12,13 @@ interface PageLayoutProps {
 export function PageLayout({ children, className = "", hideFooter = false }: PageLayoutProps) {
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <Navbar />
+      <Navbar>
+        <div className="flex items-center">
+          <Image src="/logo.png" alt="444 Exotic Rentals Logo" width={120} height={40} />
+        </div>
+      </Navbar>
       <main className={`flex-1 pt-16 ${className}`}>{children}</main>
       {!hideFooter && <Footer />}
     </div>
   )
 }
-
