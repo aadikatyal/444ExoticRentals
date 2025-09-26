1b84e8175db90c8277dcdebbc9e9e950d8518022
Aadi Katyal <aadikatyal21@gmail.com>
2025-09-25 21:28:57 -0400

    fix: implement Google-compliant favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         <script
           type="application/ld+json"
